bf8170a8a4898ed60684419a5db490015a21ca1b
abdulmuqsitzargar <abdulmuqsitzargar01@gmail.com>
2024-05-23 21:59:35 +0530

    Initial Stage of the Project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+//Custom components are always starting with upper case and html components are always lower case
+function Header(){
+    return <header>
+        <h1>Keeper </h1>
+    </header>
+}
+
+export default Header;
